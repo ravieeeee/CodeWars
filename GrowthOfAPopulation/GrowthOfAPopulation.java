8733f4eecb972983ce898a7462fcef5074ee3c7f
JY <pyeonjy97@gmail.com>
2017-08-19 22:31:42 +0900

    Day 5

---
+++ GrowthOfAPopulation/GrowthOfAPopulation.java
@@ -0,0 +1,13 @@
+
+public class GrowthOfAPopulation {
+	public static int nbYear(int p0, double percent, int aug, int p) {
+		int year = 0;
+		
+		while (p0 < p) {
+			p0 += p0 * (percent / 100) + aug;
+			year++;
+		}
+		
+		return year;
+	}
+}
